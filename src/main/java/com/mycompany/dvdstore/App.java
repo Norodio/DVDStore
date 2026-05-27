0de7533df811993b9a83eb8420e68b7d0cc1580e
Thomas <tom.arnaud97@gmail.com>
2026-05-27 17:09:32 +0200

    Exercice 4

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,6 +1,8 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
+import com.mycompany.dvdstore.repository.FileMovieRepository;
+import com.mycompany.dvdstore.service.DefaultMovieService;
 
 /**
  * Hello world!
@@ -10,6 +12,10 @@ public class App {
     public static void main(String[] args) {
 
         MovieController movieController = new MovieController();
+        DefaultMovieService movieService = new DefaultMovieService();
+        movieController.setMovieService(movieService);
+        FileMovieRepository movieRepository = new FileMovieRepository();
+        movieService.setMovieRepository(movieRepository);
         movieController.addUsingConsole();
     }
 }
--- src/main/java/com/mycompany/dvdstore/controller/MovieController.java
+++ src/main/java/com/mycompany/dvdstore/controller/MovieController.java
@@ -1,14 +1,21 @@
 package com.mycompany.dvdstore.controller;
 
 import com.mycompany.dvdstore.entity.Movie;
-import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
-import com.mycompany.dvdstore.service.MovieService;
+import com.mycompany.dvdstore.service.MovieServiceInterface;
 
 import java.util.Scanner;
 
 public class MovieController {
 
-    private MovieRepositoryInterface movieService;
+    private MovieServiceInterface movieService;
+
+    public MovieServiceInterface getMovieService() {
+        return movieService;
+    }
+
+    public void setMovieService(MovieServiceInterface movieService) {
+        this.movieService = movieService;
+    }
 
     public void addUsingConsole() {
         System.out.println("What is the movie name ?");
@@ -19,7 +26,6 @@ public class MovieController {
         Movie movie = new Movie();
         movie.setTitle(movieName);
         movie.setGenre(movieGenre);
-        MovieService movieService = new MovieService();
         movieService.registerMovie(movie);
     }
 }
